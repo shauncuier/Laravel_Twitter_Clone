9ca50f99ea6a4b9194fff281330d951a26909c29
Shaun <shauncuier@gmail.com>
2023-08-29 02:25:27 +0600

    Blade Template and Layout

---
+++ resources/views/terms.blade.php
@@ -0,0 +1,12 @@
+      @extends('layout.layout')
+
+      @section('content')
+          <h1>Terms</h1>
+          <div>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi sit ipsum culpa nulla! Quas sequi nulla
+              nesciunt adipisci repudiandae temporibus? Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem
+              cumque veniam iste natus laborum, molestias ipsa adipisci quos sed itaque nesciunt asperiores ipsam
+              numquam
+              exercitationem fugiat aperiam inventore! Dicta, similique.
+          </div>
+      @endsection
--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [DashboardController::class, 'index']);
 
 
-Route::get('/profile', [ProfileController::class, 'index']);
+Route::get('/terms', function (){
+    return view('terms');
+});
