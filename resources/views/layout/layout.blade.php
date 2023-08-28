3205a9cb9bf4b1dfe5f98b9944ca8531a1d43083
Shaun <shauncuier@gmail.com>
2023-08-29 04:28:12 +0600

    Falsh Messages

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -23,6 +23,11 @@
         @yield('content')
 
     </div>
+
+
+    <footer class="text-center">
+       <p class="m-3 h3">&copy; Copyright {{date('Y')}} | Created By Shaun</p>
+    </footer>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous">
     </script>
