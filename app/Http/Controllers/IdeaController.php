bc6625603ec13a321a074587ccd745c1275940d2
Shaun <shauncuier@gmail.com>
2023-08-30 05:44:12 +0600

    $fillable and $gurded

--- app/Http/Controllers/IdeaController.php
+++ app/Http/Controllers/IdeaController.php
@@ -9,23 +9,19 @@ class IdeaController extends Controller
 {
 
 
-    public function show(Idea $idea){
+    public function show(Idea $idea)
+    {
         return view('ideas.show', compact('idea'));
-
     }
 
 
     public function store()
 
     {
-        request()->validate([
+        $validated = request()->validate([
             'content' => 'required|min:3|max:240'
         ]);
-        $idea = Idea::create(
-            [
-                'content' => request()->get('content', ''),
-            ]
-        );
+        Idea::create($validated);
         return redirect()->route('dashboard')->with('success', 'Tweet created Successfully!');
     }
     public function destroy(Idea $idea)
@@ -35,22 +31,20 @@ class IdeaController extends Controller
         return redirect()->route('dashboard')->with('error', 'Tweet Delete Successfully!');
     }
 
-    public function edit(Idea $idea){
+    public function edit(Idea $idea)
+    {
         $editing = true;
-        return view('ideas.show', compact('idea','editing'));
-
+        return view('ideas.show', compact('idea', 'editing'));
     }
 
-    public function update(Idea $idea){
-
-        request()->validate([
+    public function update(Idea $idea)
+    {
+        $validated = request()->validate([
             'content' => 'required|min:3|max:240'
         ]);
 
-        $idea->content= request()->get('content','');
-        $idea->save();
+        $idea->update($validated);
 
-        return redirect()->route('ideas.show',$idea->id)->with('success',"Tweet Update Successfully!");
-
+        return redirect()->route('ideas.show', $idea->id)->with('success', "Tweet Update Successfully!");
     }
 }
