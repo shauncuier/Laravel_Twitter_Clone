3ff4031143e50285033c15977bcba4f74b82bf3f
Shaun <shauncuier@gmail.com>
2023-08-31 04:19:01 +0600

    User Edit

--- app/Models/User.php
+++ app/Models/User.php
@@ -43,6 +43,9 @@ class User extends Authenticatable
         'password' => 'hashed',
     ];
     public function ideas(){
-        return $this->hasMany(Idea::class);
+        return $this->hasMany(Idea::class)->latest();
+    }
+    public function comments(){
+        return $this->hasMany(Comment::class)->latest();
     }
 }
