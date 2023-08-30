da7f040843a6df27d91c2788221abdee8db4fef7
Shaun <shauncuier@gmail.com>
2023-08-31 02:56:31 +0600

    Simple Bug Fixed

--- resources/views/shared/comment-box.blade.php
+++ resources/views/shared/comment-box.blade.php
@@ -2,7 +2,7 @@
     <form action="{{ route('ideas.comments.store', $idea -> id) }}" method="post">
         @csrf
         <div class="mb-3">
-            <textarea name="content" class="fs-6 form-control" rows="1"></textarea>
+            <textarea required name="content" class="fs-6 form-control" rows="1"></textarea>
         </div>
         <div>
             <button type="submit" class="btn btn-primary btn-sm"> Post Comment </button>
